271cfe90cbea452271343ad8b6561ce62385a5ac
Wilmer Cisneros <100742212+wilmerc17@users.noreply.github.com>
2022-03-11 09:28:15 -0500

    Add files via upload

--- clase3 wsl - ubuntu/poo2.py
+++ clase3 wsl - ubuntu/poo2.py
@@ -5,7 +5,7 @@ def main():
         def __init__(self):
             self.nombre = input("ingrese el nombre: ")
             self.apellido = input("ingrese el apellido: ")
-            self.direccion = input("ingrese la direccin: ")
+            self.direccion = input("ingrese la direccion: ")
             self.telefono = input("ingrese el telefono: ")
 
         def mostrarPersona(self):
@@ -23,15 +23,38 @@ def main():
         def __init__(self):
             super().__init__()
             self.__sueldo = float(input("ingrese el sueldo: "))
-            self.alimentacion = 0
-            self.transporte = 0
-            self.pension = 0
+
+            
+            if self.__pension < 2000000:
+                self.salud = 80000
+                self.transporte = 60000
+
+            else:
+                self.alimentacion = 0
+                self.transporte = 0
+
+            self.pension = self.__sueldo *0.04
+            self.salud = self.__sueldo *0.0375
+
+            self.dev
+        
+            def calcularDevengado():
+            dev = self.alimentacion + self.transporte
+            return dev
+
+            def calcularDeducciones():
+                ded = self.pension + self.salud
+                return ded
+
+
         def mostrarEmpleado(self):
             super().mostrarPersona()
             print(f"sueldo: {self.__sueldo}")
-            print(f"transporte: {self.transporte}")
-            print(f"alimentacion: {self.alimentacion}")
-            print(f"pension: {self.pension}")
+            print(f"devengado: {self.dev}")
+            print(f"deducciones: {self.ded}")
+            print(f"total a pagar: {self.__sueldo + self.dev + self.ded}")
+
+       
 
     Empleado1 = Empleado()
     #Empleado1.__sueldo = 4000000
